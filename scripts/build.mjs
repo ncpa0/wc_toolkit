2994dfcb4dd3d3d3f153008e5b433051c64b3513
ncpa0cpl <szymonb21@gmail.com>
2025-07-30 16:23:42 +0200

    chore: rebuild on publish

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,14 +1,23 @@
 import { build } from "@ncpa0cpl/nodepack";
+import { exec as execCb } from "node:child_process";
 import path from "node:path";
+import util from "node:util";
 import { fileURLToPath } from "url";
+
+const exec = util.promisify(execCb);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const p = (...fpath) => path.resolve(__dirname, "..", ...fpath);
 
 const isDev = process.argv.includes("--dev");
 const watch = process.argv.includes("--watch");
+const clean = process.argv.includes("--clean");
 
 async function main() {
+  if (clean) {
+    await exec("rm -rf ./dist");
+  }
+
   /**
    * @type {import("@ncpa0cpl/nodepack").BuildConfig}
    */
